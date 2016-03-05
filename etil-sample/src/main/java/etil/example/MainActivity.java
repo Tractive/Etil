f07e5637befb80be8faa36d5fc6f295fc6f55b26
Stephan Hener <stephan.hener@tractive.com>
2016-03-05 23:18:24 +0100

    FIX missing jcenter() for gradle beta6 build tools RM supportv4 stuff

--- etil-sample/src/main/java/etil/example/MainActivity.java
+++ etil-sample/src/main/java/etil/example/MainActivity.java
@@ -1,6 +1,5 @@
 package etil.example;
 
-import com.tractive.android.etil.R;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -11,8 +10,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
 
     }
 }
